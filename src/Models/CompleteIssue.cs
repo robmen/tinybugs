3fcf9bae9066b9fe5ec94e1f71b60c0722113a55
Rob Mensching <rob@robmensching.com>
2013-07-25 17:32:29 -0700

    Cleanup namespaces.

--- src/Models/CompleteIssue.cs
+++ src/Models/CompleteIssue.cs
@@ -2,10 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using ServiceStack.DataAnnotations;
-    using ServiceStack.OrmLite;
 
     public class CompleteIssue
     {
