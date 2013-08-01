23af3c2a03d1deaf1136392bad8b9c8d92b984ca
Rob Mensching <rob@robmensching.com>
2013-08-01 14:00:13 -0700

    Add releases to and pretty up root page.

--- src/ViewModels/AppViewModel.cs
+++ src/ViewModels/AppViewModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace RobMensching.TinyBugs.ViewModels
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using RobMensching.TinyBugs.Services;
 
     public class AppViewModel
@@ -7,5 +9,7 @@
         public string Path { get { return ConfigService.AppPath; } }
 
         public string Name { get { return ConfigService.AppName; } }
+
+        public IEnumerable<ReleaseViewModel> Releases { get { return ConfigService.Releases.Select(r => new ReleaseViewModel() { Name = r }); } }
     }
 }
---
+++ src/ViewModels/ReleaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace RobMensching.TinyBugs.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class ReleaseViewModel
+    {
+        public string Name { get; set; }
+
+        public int IssueCount { get; set; }
+    }
+}
