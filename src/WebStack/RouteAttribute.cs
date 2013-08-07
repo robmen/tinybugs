fff3c25dc15865a4bff98288bb1429ffa76da33a
Rob Mensching <rob@robmensching.com>
2013-08-07 00:52:57 -0700

    Initial attempt to add defaults to routes. Needs more work.

--- src/WebStack/RouteAttribute.cs
+++ src/WebStack/RouteAttribute.cs
@@ -1,6 +1,7 @@
 ﻿namespace RobMensching.TinyWebStack
 {
     using System;
+    using System.Web.Routing;
 
     [Flags]
     public enum RouteVerbs
@@ -10,20 +11,23 @@
         Post = 0x2,
         Put = 0x4,
         Delete = 0x8,
-        //Patch = 0x10,
-        //Head = 0x20,
+        Patch = 0x10,
+        Head = 0x20,
     }
 
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
     public class RouteAttribute : Attribute
     {
-        public RouteAttribute(string path, RouteVerbs verbs = RouteVerbs.Get | RouteVerbs.Post | RouteVerbs.Put | RouteVerbs.Delete)
+        public RouteAttribute(string path)
         {
             this.Path = path;
-            this.Verbs = verbs;
+            this.Defaults = null;
+            this.Verbs = RouteVerbs.Get | RouteVerbs.Post | RouteVerbs.Put | RouteVerbs.Delete;
         }
 
         public string Path { get; set; }
+
+        public RouteValueDictionary Defaults { get; set; }
 
         public RouteVerbs Verbs { get; set; }
     }
