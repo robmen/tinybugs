36bd587354d085f91dd3b78455567dac55915193
Rob Mensching <rob@robmensching.com>
2013-08-11 14:43:37 -0700

    Fix #14 - embed tinyBugs assets and allow override at 'assets/custom'.

--- src/Services/FileService.cs
+++ src/Services/FileService.cs
@@ -1,7 +1,10 @@
 ﻿namespace RobMensching.TinyBugs.Services
 {
+    using System;
     using System.Collections.Concurrent;
     using System.IO;
+    using System.Resources;
+    using System.Reflection;
     using Nustache.Core;
     using RobMensching.TinyBugs.ViewModels;
 
@@ -14,12 +17,24 @@
             Template template;
             if (!CompiledTemplates.TryGetValue(path, out template))
             {
-                string assetPath = Path.Combine("assets", path);
-
                 template = new Template();
-                using (var reader = FileService.ReadFile(assetPath))
+
+                string assetPath = Path.Combine(ConfigService.RootPath, "assets\\custom", path);
+                if (File.Exists(assetPath))
                 {
-                    template.Load(reader);
+                    using (var reader = FileService.ReadFile(assetPath))
+                    {
+                        template.Load(reader);
+                    }
+                }
+                else
+                {
+                    string assetStream = String.Concat("RobMensching.TinyBugs.assets.", path.Replace('/', '.').Replace('\\', '.'));
+                    using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(assetStream))
+                    {
+                        StreamReader s = new StreamReader(stream);
+                        template.Load(s);
+                    }
                 }
 
                 //if (!CompiledTemplates.TryAdd(path, template))
@@ -70,18 +85,54 @@
         public static void PregenerateApp()
         {
             var vm = new { app = new AppViewModel(), breadcrumbs = ConfigService.ExternalBreadcrumbs, mail = ConfigService.Mail };
-            var foos = new[] { new { template = "tinybugs.js.mustache", output = "assets/js/tinybugs.js" },
-                               new { template = "accessdenied.mustache", output = "accessdenied/index.html" },
-                               new { template = "admin.mustache", output = "admin/index.html" },
-                               new { template = "login.mustache", output = "login/index.html" },
-                               new { template = "login_create.mustache", output = "login/create/index.html" },
-                               new { template = "login_reset.mustache", output = "login/reset/index.html" },
-                               new { template = "login_activate.mustache", output = "login/activate/index.html" },
-                               new { template = "bugform.mustache", output = "new/index.html" }, };
-            foreach (var foo in foos)
+            var pregens = new[] { new { template = "accessdenied.mustache", output = "accessdenied/index.html" },
+                                  new { template = "admin.mustache", output = "admin/index.html" },
+                                  new { template = "login.mustache", output = "login/index.html" },
+                                  new { template = "login_create.mustache", output = "login/create/index.html" },
+                                  new { template = "login_reset.mustache", output = "login/reset/index.html" },
+                                  new { template = "login_activate.mustache", output = "login/activate/index.html" },
+                                  new { template = "bugform.mustache", output = "new/index.html" },
+                                  new { template = "tinybugs.css.mustache", output = "assets/css/tinybugs.css" },
+                                  new { template = "tinybugs.js.mustache", output = "assets/js/tinybugs.js" }, };
+            foreach (var pregen in pregens)
             {
-                Template template = LoadTemplate(foo.template);
-                RenderTemplateToFile(template, vm, Path.Combine(ConfigService.RootPath, foo.output));
+                Template template = LoadTemplate(pregen.template);
+                RenderTemplateToFile(template, vm, Path.Combine(ConfigService.RootPath, pregen.output));
+            }
+
+            ExtractMarkdownEditor();
+        }
+
+        private static void ExtractMarkdownEditor()
+        {
+            string outputFolder = Path.Combine(ConfigService.RootPath, "assets/mdd");
+            Directory.CreateDirectory(outputFolder);
+
+            var files = new[] { "markdowndeeplib.min.js",
+                               "mdd_ajax_loader.gif",
+                               "mdd_gripper.png",
+                               "mdd_help.htm",
+                               "mdd_modal_background.png",
+                               "mdd_styles.css",
+                               "mdd_toolbar.png", };
+            foreach (var file in files)
+            {
+                string outputPath = Path.Combine(ConfigService.RootPath, "assets\\mdd", file);
+                if (!File.Exists(outputFolder))
+                {
+                    string streamName = String.Concat("RobMensching.TinyBugs.assets.mdd.", file);
+
+                    using (var input = Assembly.GetExecutingAssembly().GetManifestResourceStream(streamName))
+                    using (var output = File.Create(outputPath))
+                    {
+                        int read = 0;
+                        byte[] buffer = new byte[1024];
+                        while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            output.Write(buffer, 0, read);
+                        }
+                    }
+                }
             }
         }
     }
