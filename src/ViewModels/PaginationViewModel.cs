bd7e0e1c2481758894667542f2bbbeb9c69e376b
Rob Mensching <rob@robmensching.com>
2013-08-14 11:48:52 -0700

    Add improved pagination to search page.

--- src/ViewModels/PaginationViewModel.cs
+++ src/ViewModels/PaginationViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace RobMensching.TinyBugs.ViewModels
 {
     using System;
+    using System.Collections.Generic;
 
     public class PaginationViewModel
     {
@@ -10,12 +11,23 @@
             this.ItemsPerPage = perPage;
             this.ItemsTotal = totalItems;
             this.Total = (totalItems + perPage - 1) / perPage;
-            this.Previous = (page > 1) ? Math.Min(page - 1, this.Total).ToString() : null;
-            this.Next = (page < this.Total) ? (page + 1).ToString() : null;
 
             this.PagedUrlPrefix = pagedUrlPrefix;
-            this.PreviousUrl = (!String.IsNullOrEmpty(this.Previous) && !String.IsNullOrEmpty(this.PagedUrlPrefix)) ? this.PagedUrlPrefix + this.Previous : null;
-            this.NextUrl = (!String.IsNullOrEmpty(this.Next) && !String.IsNullOrEmpty(this.PagedUrlPrefix)) ? this.PagedUrlPrefix + this.Next : null;
+
+            int width = 10;
+            int start = Math.Max(1, this.Current - width / 2);
+            int end = Math.Min(this.Total, start + width);
+
+            this.First = new Page() { Number = 1, Active = false, Disabled = (page == 1), Url = !String.IsNullOrEmpty(this.PagedUrlPrefix) ? this.PagedUrlPrefix + "1" : null, };
+            this.Previous = new Page() { Number = 1, Active = false, Disabled = (page == 1), Url = !String.IsNullOrEmpty(this.PagedUrlPrefix) ? this.PagedUrlPrefix + Math.Min(page - 1, this.Total) : null, };
+            this.Next = new Page() { Number = Math.Min(page + 1, this.Total), Active = false, Disabled = (page == this.Total), Url = !String.IsNullOrEmpty(this.PagedUrlPrefix) ? this.PagedUrlPrefix + Math.Min(page + 1, this.Total) : null, };
+            this.Last = new Page() { Number = this.Total, Active = false, Disabled = (page == this.Total), Url = !String.IsNullOrEmpty(this.PagedUrlPrefix) ? this.PagedUrlPrefix + this.Total : null, };
+
+            this.Pages = new List<Page>(end - start);
+            for (int i = start; i <= end; ++i)
+            {
+                this.Pages.Add(new Page() { Number = i, Active = (this.Current == i), Url = !String.IsNullOrEmpty(this.PagedUrlPrefix) ? this.PagedUrlPrefix + i : null, });
+            }
         }
 
         public int Current { get; set; }
@@ -26,14 +38,27 @@
 
         public int ItemsTotal { get; set; }
 
+        public List<Page> Pages { get; private set; }
+
         public string PagedUrlPrefix { get; set; }
 
-        public string Previous { get; set; }
+        public Page First { get; set; }
 
-        public string PreviousUrl { get; set; }
+        public Page Previous { get; set; }
 
-        public string Next { get; set; }
+        public Page Next { get; set; }
 
-        public string NextUrl { get; set; }
+        public Page Last { get; set; }
+
+        public class Page
+        {
+            public int Number { get; set; }
+
+            public bool Active { get; set; }
+
+            public bool Disabled { get; set; }
+
+            public string Url { get; set; }
+        }
     }
 }
