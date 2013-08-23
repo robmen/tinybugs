71a1d39f9685be8cda447bb56956679f4daa7701
Rob Mensching <rob@robmensching.com>
2013-08-23 16:30:42 -0700

    Rename edit user to 'edit me' which is more correct.

--- src/Controllers/EditUserController.cs
+++ src/Controllers/EditUserController.cs
@@ -7,18 +7,18 @@
     using RobMensching.TinyBugs.Views;
     using RobMensching.TinyWebStack;
 
-    [Route("edit/user")]
+    [Route("edit/me")]
     public class EditUserController : ControllerBase
     {
         public override ViewBase Get(ControllerContext context)
         {
-            User user;
-            if (!UserService.TryAuthenticateUser(context.User, out user))
+            User me;
+            if (!UserService.TryAuthenticateUser(context.User, out me))
             {
                 return new StatusCodeView(HttpStatusCode.Unauthorized);
             }
 
-            return new TemplateView("userform.mustache", new { app = new AppViewModel(), user = new UserViewModel(user), });
+            return new TemplateView("userform.mustache", new { app = new AppViewModel(), user = new UserViewModel(me), });
         }
     }
 }
